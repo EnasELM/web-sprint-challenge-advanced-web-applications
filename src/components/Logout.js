3a45bd68528f7de5c07972648ef2486febcdd40a
Enas <enaselmahdwi@gmail.com>
2021-11-12 09:28:15 -0700

    build Logout file

--- src/components/Logout.js
+++ src/components/Logout.js
@@ -1,6 +1,21 @@
-import React from 'react';
+import React, {useEffect} from 'react';
+import { useHistory } from 'react-router';
 
-const Logout = () => {        
+import axiosWithAuth from '../utils/axiosWithAuth';
+
+const Logout = (props) => {  
+    const { push } = useHistory();
+
+    useEffect(()=> {
+        const token = localStorage.getItem("token");
+        
+        axiosWithAuth()
+            .post('/logout')
+            .then(resp => {                
+                localStorage.removeItem('token');
+                push('/login');
+            });
+    }, []);      
     return(<div></div>);
 }
 
